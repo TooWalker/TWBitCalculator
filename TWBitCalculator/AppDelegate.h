f62e6293fc123027242068f6f5c7134c5e6aaf72
lei.dai <lei.dai@qunar.com>
2016-06-20 10:33:02 +0800

    Initial Commit

---
+++ TWBitCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TWBitCalculator
+//
+//  Created by TooWalker on 6/20/16.
+//  Copyright © 2016 TooWalker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TWBitCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TWBitCalculator
+//
+//  Created by TooWalker on 6/20/16.
+//  Copyright © 2016 TooWalker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
